0612c8e3c41b5685019c1d31ca49343047c6e606
Joan Miquel Espada Sabat <joanmi@espada.cat>
2018-04-10 23:22:28 +0200

    dotenv configuration

--- API/src/index.js
+++ API/src/index.js
@@ -1,12 +1,16 @@
 import http from 'http';
 import option from './option';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
   let aux = new option();
 
-  res.end('Hello World ' + aux.abc() );
+  res.end('Hello World ' + process.env.FIREBASE_PRIVATE_KEY );
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
