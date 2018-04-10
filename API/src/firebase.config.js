99dfac4a9680405dc6b12bc00e387f18d397e0c1
Joan Miquel Espada Sabat <joanmi@espada.cat>
2018-04-11 00:53:31 +0200

    firebase config and easy read

---
+++ API/src/firebase.config.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import * as admin from 'firebase-admin';
+
+dotenv.config();
+var private_key_cert = process.env.FIREBASE_PRIVATE_KEY_CERT.replace(/\\n/g, '\n');
+
+let config ={
+    "type": "service_account",
+    "project_id": "pmp-empowered",
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": private_key_cert,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ja8r7%40pmp-empowered.iam.gserviceaccount.com"
+  }
+
+ 
+admin.initializeApp({
+  credential: admin.credential.cert(config),
+  databaseURL: 'https://pmp-empowered.firebaseio.com'
+});
+
+let db = admin.firestore();
+
+export default db;
+
+
+
--- API/src/index.js
+++ API/src/index.js
@@ -1,16 +1,22 @@
 import http from 'http';
 import option from './option';
 
-import dotenv from 'dotenv';
-dotenv.config();
-
+import * as firebase from './firebase.config';
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  let aux = new option();
-
-  res.end('Hello World ' + process.env.FIREBASE_PRIVATE_KEY );
+  firebase.default.collection('users').get()
+    .then((snapshot) => {
+        let str ='';
+        snapshot.forEach((doc) => {
+            str += doc.id, '=>', doc.data(); 
+        });
+        res.end(str);
+    })
+    .catch((err) => {
+        console.log('Error getting documents', err);
+    });
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
