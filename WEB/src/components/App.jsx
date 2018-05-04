d1c246bc36a84b1ac30f5c3134a68ac123683da4
Joan Miquel Espada Sabat <joanmi@espada.cat>
2018-05-04 14:35:25 +0200

    login window with 100% react mindset

--- WEB/src/components/App.jsx
+++ WEB/src/components/App.jsx
@@ -1,50 +1,62 @@
-import React, { Component } from 'react'
-import { AppBar, Checkbox, IconButton } from 'react-toolbox'
-import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox'
-import GithubIcon from './githubicon'
-import Login from './login'
+import React, { Component } from 'react';
+import { AppBar, IconButton, Layout, NavDrawer, Panel, Sidebar, FontIcon } from 'react-toolbox';
+import Login from './login';
 
 class App extends Component {
-  state = {
-    drawerActive: false,
-    drawerPinned: false,
-    sidebarPinned: false,
-    loginDialog: false
-  };
-toggleDrawerActive = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawerActive: false,
+      drawerPinned: false,
+      sidebarPinned: false,
+      loginDialog: false,
+    };
+  }
+
+  componentDidMount() {
+    console.log('I was triggered during componentDidMount');
+  }
+
+  toggleDrawerActive = () => {
     this.setState({ drawerActive: !this.state.drawerActive });
   };
-toggleDrawerPinned = () => {
+  toggleDrawerPinned = () => {
     this.setState({ drawerPinned: !this.state.drawerPinned });
   };
-toggleSidebar = () => {
+  toggleSidebar = () => {
     this.setState({ sidebarPinned: !this.state.sidebarPinned });
   };
-loginOpen = () => {
+  loginOpen = () => {
     this.setState({ loginDialog: !this.state.loginDialog });
-    console.log(this.state)
-}
-render() {
+  }
+
+  render() {
     return (
       <Layout>
-        <Login  showme={this.state.loginDialog } />
-        <NavDrawer active={this.state.drawerActive}
-                    permanentAt='xxxl'
-                   onOverlayClick={ this.toggleDrawerActive }>
+        <Login showme={this.state.loginDialog} handleToggle={this.loginOpen} />
+        <NavDrawer
+          active={this.state.drawerActive}
+          permanentAt="xxxl"
+          onOverlayClick={this.toggleDrawerActive}
+        >
           <p>
             Navigation, account switcher, etc. go here.
           </p>
         </NavDrawer>
         <Panel>
-          <AppBar title="Pmp enhanced" leftIcon="menu" rightIcon={<GithubIcon/>} 
-                    onLeftIconClick={ this.toggleDrawerActive } onRightIconClick={this.loginOpen}   />
+          <AppBar
+            title="Pmp enhanced"
+            leftIcon="menu"
+            rightIcon={<FontIcon value="account_circle" />}
+            onLeftIconClick={this.toggleDrawerActive}
+            onRightIconClick={this.loginOpen}
+          />
           <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
             <h1>Welcome</h1>
-            
           </div>
         </Panel>
-        <Sidebar pinned={ this.state.sidebarPinned } width={ 5 }>
-          <div><IconButton icon='close' onClick={ this.toggleSidebar }/></div>
+        <Sidebar pinned={this.state.sidebarPinned} width={5}>
+          <div><IconButton icon="close" onClick={this.toggleSidebar} /></div>
           <div style={{ flex: 1 }}>
             <p>Supplemental content goes here.</p>
           </div>
@@ -53,4 +65,5 @@ render() {
     );
   }
 }
-export default App
+
+export default App;
--- WEB/src/components/login.jsx
+++ WEB/src/components/login.jsx
@@ -1,37 +1,87 @@
-import React, { Component } from 'react'
-import Dialog from 'react-toolbox/lib/dialog'
+import React from 'react';
+import { Dialog, Input } from 'react-toolbox';
+import PropTypes from 'prop-types';
 
-class Login extends React.Component
-{
-    constructor(props) {
-        super(props);
-        this.state = {
-          active: props.showme
-        };
-      }
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      passw: '',
+      formValid: false,
+      formErrors: { email: undefined, passw: undefined },
+    };
+  }
 
-    handleToggle = () => {
-        this.setState({active: !this.state.active});
+  handleChange = name => (value) => {
+    this.setState({ ...this.state, [name]: value }, () => {
+      const validate = (email) => {
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      };
+      const valid = (validate(this.state.email) && this.state.passw.length > 0) ? true : false;
+      this.setState({ ...this.state, formValid: valid });
+    });
+  }
 
+  login = () => {
+  }
+
+  render() {
+    const errorMessage = [];
+    const red = { color: 'red' };
+
+    if (this.state.formErrors.email !== undefined) {
+      errorMessage.push((<span style={red}>{this.state.formErrors.email} </span>));
+    }
+    if (this.state.formErrors.passw !== undefined) {
+      errorMessage.push((<span style={red}> {this.state.formErrors.email} </span>));
     }
 
-    actions = [
-        { label: "Login", onClick: this.handleToggle },
-        { label: "Close", onClick: this.handleToggle }
-      ];
- 
-    render() {
-        return (<Dialog
-          actions={this.actions}
-          active={this.state.active}
-          onEscKeyDown={this.handleToggle}
-          onOverlayClick={this.handleToggle}
-          title='Login'
-        >
-          <p>Here you can add arbitrary content. Components like Pickers are using dialogs now.</p>
-        </Dialog>)
-    }
+    const actions = [
+      {
+        label: 'Login',
+        disabled: !this.state.formValid,
+        raised: true,
+        primary: true,
+        onClick: this.login,
+      },
+      { label: 'Close', onClick: this.props.handleToggle },
+    ];
+
+    return (
+      <Dialog
+        actions={actions}
+        active={this.props.showme}
+        onEscKeyDown={this.props.handleToggle}
+        onOverlayClick={this.props.handleToggle}
+        title="Login"
+      >
+        <Input
+          type="email"
+          label="Email address"
+          required
+          value={this.state.email}
+          maxLength={50}
+          onChange={this.handleChange('email')}
+        />
+        <Input
+          type="password"
+          label="Password"
+          required
+          value={this.state.passw}
+          maxLength={50}
+          onChange={this.handleChange('passw')}
+        />
+        {errorMessage}
+      </Dialog>);
+  }
 }
 
-export default Login
+Login.propTypes = {
+  showme: PropTypes.string.isRequired,
+  handleToggle: PropTypes.func.isRequired,
+};
+
+export default Login;
 
