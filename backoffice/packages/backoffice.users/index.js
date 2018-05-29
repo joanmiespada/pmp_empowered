9013e24ba2932ecf81e41ddfc7b73b73754b07ac
Joan Miquel Espada Sabat <joanmi@espada.cat>
2018-05-30 00:13:33 +0200

    WIP

--- backoffice/packages/backoffice.users/index.js
+++ backoffice/packages/backoffice.users/index.js
@@ -1,4 +1,11 @@
-export * from './src/actions'
-export * from './src/reducers'
-export * from './src/view'
-export * from './src/model'
+import * as actions from './src/actions'
+import * as reducers  from './src/reducers'
+import * as view from './src/view'
+import * as model from './src/model'
+
+export {
+    actions,
+    reducers,
+    view,
+    model
+}
--- backoffice/packages/backoffice.users/src/view.jsx
+++ backoffice/packages/backoffice.users/src/view.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Dialog, Input, ProgressBar } from 'react-toolbox';
-import { Table } from 'react-toolbox';
-import PropTypes from 'prop-types';
-import { login } from './actions';
+//import { connect } from 'react-redux';
+// import { Dialog, Input, ProgressBar } from 'react-toolbox';
+//import { Table } from 'react-toolbox';
+// import PropTypes from 'prop-types';
+// import { login } from './actions';
 
-
+/*
 const UserModel = {
-  name: {type: String},
-  twitter: {type: String},
-  birthdate: {type: Date,
-    title: 'Date of Birth'},
-  cats: {type: Number},
-  dogs: {type: Number},
-  active: {type: Boolean}
+  name: { type: String },
+  twitter: { type: String },
+  birthdate: { type: Date, title: 'Date of Birth' },
+  cats: { type: Number },
+  dogs: { type: Number },
+  active: { type: Boolean },
 };
-
-const users = [
-  {name: 'Javi Jimenez', twitter: '@soyjavi', birthdate: new Date(1980, 3, 11), cats: 1},
-  {name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true}
+*/
+const users = [{
+    name: 'Javi Jimenez', twitter: '@soyjavi', birthdate: new Date(1980, 3, 11), cats: 1,
+},
+  { name: 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true }
 ];
 
 
@@ -26,7 +26,7 @@ export class View extends React.Component {
     constructor() {
         super()
         this.state = { selected: [], source: users }
-    }
+    }/*
     handleChange = (row, key, value) => {
         const source = this.state.source;
         source[row][key] = value;
@@ -36,10 +36,18 @@ export class View extends React.Component {
     handleSelect = (selected) => {
         this.setState({selected});
     };
-
+*/
     render() {
         return (
-        <Table
+            <div>pepepe</div>
+        )
+    }
+}
+
+
+/*
+
+<Table
             model={UserModel}
             onChange={this.handleChange}
             onSelect={this.handleSelect}
@@ -48,6 +56,5 @@ export class View extends React.Component {
             selected={this.state.selected}
             source={this.state.source}
         />
-        )
-    }
-}
+
+*/
